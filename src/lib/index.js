b696466212dc6ff00014398f82f44a8a0b005d4f
Orry Baram <orrybaram@gmail.com>
2018-01-04 18:43:54 -0500

    feat(Lib): adds frontiers delegators and keys to index

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,6 +2,9 @@ import account from './Account';
 import accounts from './Accounts';
 import blocks from './Blocks';
 import bootstrap from './Bootstrap';
+import delegators from './Delegators';
+import frontiers from './Frontiers';
+import keys from './Keys';
 import wallet from './Wallet';
 
 export default {
@@ -9,5 +12,8 @@ export default {
   accounts,
   blocks,
   bootstrap,
+  delegators,
+  frontiers,
+  keys,
   wallet,
 };
