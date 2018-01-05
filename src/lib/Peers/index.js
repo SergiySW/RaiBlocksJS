e88355087d1ef34b90f4b7593829416a501e1cba
orrybaram <orrybaram@gmail.com>
2018-01-05 17:46:49 -0500

    feat(Peers): fills out Peer methods

--- src/lib/Peers/index.js
+++ src/lib/Peers/index.js
@@ -1,7 +1,25 @@
-export default function Peers(rpc) {
+import getSuccessResponse from '../../utils/getSuccessResponse';
 
+export default function Peers(rpc) {
+  const addWorkPeer = async ({ address, port }) => {
+    const response = await rpc('work_peer_add', { address, port });
+    return getSuccessResponse(response);
+  };
+
+  const getWorkPeers = () => rpc('work_peers');
+
+  const clearWorkPeers = async () => {
+    const response = await rpc('work_peers_clear');
+    if (response) return { success: true };
+    return { success: false };
+  };
+
+  const get = () => rpc('peers');
 
   return {
-
+    addWorkPeer,
+    getWorkPeers,
+    clearWorkPeers,
+    get,
   };
 }
--- src/lib/Peers/test.js
+++ src/lib/Peers/test.js
@@ -2,88 +2,76 @@ import mockServer from '../../../test/mockServer';
 import rai from '../../../test/mockRai';
 
 describe('Peers', () => {
-  test('peers.begin', async () => {
-    const expected = {
-      account: 'xrb_3e3j5tkog48pnny9dmfzj1r16pg8t1e76dz5tmac6iq689wyjfpi00000000'
-    };
-
-    const request = {
-      wallet: '000D1BAEC8EC208142C99059B393051BAC8380F9B5A2E6B2489A277D81789F3F'
-    };
-
-    mockServer.success({
-      request: Object.assign({}, request, {
-        action: 'peers_begin',
-      }),
-      response: expected,
-    });
-
-    const response = await rai.peers.begin(request);
-    expect(response).toEqual(expected);
-  });
-
-  test('peers.init', async () => {
-    const expected = {
-      status: 'Ready',
-    };
-
-    const request = {
-      wallet: '000D1BAEC8EC208142C99059B393051BAC8380F9B5A2E6B2489A277D81789F3F'
-    };
-
-    mockServer.success({
-      request: Object.assign({}, request, {
-        action: 'peers_init',
-      }),
-      response: expected,
-    });
-
-    const response = await rai.peers.init(request);
-    expect(response).toEqual(expected);
-  });
-
-  test('peers.end', async () => {
+  test('peers.addWorkPeer', async () => {
     const expected = {
       success: true,
     };
 
     const request = {
-      wallet: '000D1BAEC8EC208142C99059B393051BAC8380F9B5A2E6B2489A277D81789F3F',
-      account: 'xrb_3e3j5tkog48pnny9dmfzj1r16pg8t1e76dz5tmac6iq689wyjfpi00000000'
+      address: '::ffff:172.17.0.1:7076',
+      port: '7076',
     };
 
     mockServer.success({
       request: Object.assign({}, request, {
-        action: 'peers_end',
+        action: 'work_peer_add',
       }),
+      response: { success: '' },
+    });
+
+    const response = await rai.peers.addWorkPeer(request);
+    expect(response).toEqual(expected);
+  });
+
+  test('peers.getWorkPeers', async () => {
+    const expected = {
+      work_peers: [
+        '::ffff:172.17.0.1:7076',
+      ],
+    };
+
+    mockServer.success({
+      request: {
+        action: 'work_peers',
+      },
+      response: expected,
+    });
+
+    const response = await rai.peers.getWorkPeers();
+    expect(response).toEqual(expected);
+  });
+
+  test('peers.clearWorkPeers', async () => {
+    const expected = {
+      success: true,
+    };
+
+    mockServer.success({
+      request: {
+        action: 'work_peers_clear',
+      },
       response: {},
     });
 
-    const response = await rai.peers.end(request);
+    const response = await rai.peers.clearWorkPeers();
     expect(response).toEqual(expected);
   });
 
-  test('peers.wait', async () => {
+  test('peers.get', async () => {
     const expected = {
-      success: true,
-    };
-
-    const request = {
-      account: 'xrb_3e3j5tkog48pnny9dmfzj1r16pg8t1e76dz5tmac6iq689wyjfpi00000000',
-      amount: '1',
-      timeout: '1000',
+      peers: [
+        '::ffff:172.17.0.1:7076',
+      ],
     };
 
     mockServer.success({
-      request: Object.assign({}, request, {
-        action: 'peers_wait',
-      }),
-      response: {
-        status: 'success',
+      request: {
+        action: 'peers',
       },
+      response: expected,
     });
 
-    const response = await rai.peers.wait(request);
+    const response = await rai.peers.get();
     expect(response).toEqual(expected);
   });
 });
