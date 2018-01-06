c2410abcadce34a91438fcfb14b88f24be062676
orrybaram <orrybaram@gmail.com>
2018-01-06 12:26:59 -0500

    feat(Representatives): adds methods and tests

--- src/lib/Representatives/test.js
+++ src/lib/Representatives/test.js
@@ -2,88 +2,64 @@ import mockServer from '../../../test/mockServer';
 import rai from '../../../test/mockRai';
 
 describe('Representatives', () => {
-  test('representatives.begin', async () => {
+  test('representatives.get', async () => {
     const expected = {
-      account: 'xrb_3e3j5tkog48pnny9dmfzj1r16pg8t1e76dz5tmac6iq689wyjfpi00000000'
+      representatives: {
+        xrb_1111111111111111111111111111111111111111111111111117353trpda: '3822372327060170000000000000000000000',
+        xrb_1111111111111111111111111111111111111111111111111awsq94gtecn: '30999999999999999999999999000000',
+        xrb_114nk4rwjctu6n6tr6g6ps61g1w3hdpjxfas4xj1tq6i8jyomc5d858xr1xi: '0',
+      },
+    };
+
+    mockServer.success({
+      request: {
+        action: 'representatives',
+      },
+      response: expected,
+    });
+
+    const response = await rai.representatives.get();
+    expect(response).toEqual(expected);
+  });
+
+  test('representatives.wallet', async () => {
+    const expected = {
+      representative: 'xrb_1111111111111111111111111111111111111111111111111117353trpda',
     };
 
     const request = {
-      wallet: '000D1BAEC8EC208142C99059B393051BAC8380F9B5A2E6B2489A277D81789F3F'
+      wallet: '000D1BAEC8EC208142C99059B393051BAC8380F9B5A2E6B2489A277D81789F3F',
     };
 
     mockServer.success({
       request: Object.assign({}, request, {
-        action: 'representatives_begin',
+        action: 'wallet_representative',
       }),
       response: expected,
     });
 
-    const response = await rai.representatives.begin(request);
+    const response = await rai.representatives.wallet(request);
     expect(response).toEqual(expected);
   });
 
-  test('representatives.init', async () => {
-    const expected = {
-      status: 'Ready',
-    };
-
-    const request = {
-      wallet: '000D1BAEC8EC208142C99059B393051BAC8380F9B5A2E6B2489A277D81789F3F'
-    };
-
-    mockServer.success({
-      request: Object.assign({}, request, {
-        action: 'representatives_init',
-      }),
-      response: expected,
-    });
-
-    const response = await rai.representatives.init(request);
-    expect(response).toEqual(expected);
-  });
-
-  test('representatives.end', async () => {
+  test('representatives.walletSet', async () => {
     const expected = {
       success: true,
     };
 
     const request = {
       wallet: '000D1BAEC8EC208142C99059B393051BAC8380F9B5A2E6B2489A277D81789F3F',
-      account: 'xrb_3e3j5tkog48pnny9dmfzj1r16pg8t1e76dz5tmac6iq689wyjfpi00000000'
+      representative: 'xrb_3e3j5tkog48pnny9dmfzj1r16pg8t1e76dz5tmac6iq689wyjfpi00000000'
     };
 
     mockServer.success({
       request: Object.assign({}, request, {
-        action: 'representatives_end',
+        action: 'wallet_representative_set',
       }),
-      response: {},
+      response: { set: '1' },
     });
 
-    const response = await rai.representatives.end(request);
-    expect(response).toEqual(expected);
-  });
-
-  test('representatives.wait', async () => {
-    const expected = {
-      success: true,
-    };
-
-    const request = {
-      account: 'xrb_3e3j5tkog48pnny9dmfzj1r16pg8t1e76dz5tmac6iq689wyjfpi00000000',
-      amount: '1',
-      timeout: '1000',
-    };
-
-    mockServer.success({
-      request: Object.assign({}, request, {
-        action: 'representatives_wait',
-      }),
-      response: {
-        status: 'success',
-      },
-    });
-
-    const response = await rai.representatives.wait(request);
+    const response = await rai.representatives.walletSet(request);
     expect(response).toEqual(expected);
   });
 });
