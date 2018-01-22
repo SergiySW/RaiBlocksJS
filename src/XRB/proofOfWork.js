7e104dd100b353dec2caf451806fa4236226d831
orrybaram <orrybaram@gmail.com>
2018-01-21 19:27:05 -0500

    refactor(pow) lints pow and adds in scripts

--- src/XRB/proofOfWork.js
+++ src/XRB/proofOfWork.js
@@ -1,10 +1,53 @@
+import { blake2bInit, blake2bUpdate, blake2bFinal } from 'blakejs';
+import cpus from 'cpus';
+import workerPool from 'workerpool';
 import { isValidHash } from './utils';
+import { hexToUint8 } from '../utils/uint';
+
+function threshold(Uint8Array) {
+  return (
+    Uint8Array[0] === 255
+    && Uint8Array[1] === 255
+    && Uint8Array[2] === 255
+    && Uint8Array[3] >= 192
+  );
+}
+
+function randomUint() {
+  const array = new Uint8Array(8);
+  self.crypto.getRandomValues(array);
+  return array;
+}
+
+function generator256(hash) {
+  const random = randomUint();
+  for (let r = 0; r < 256; r += 1) {
+    random[7] = (random[7] + r) % 256; // pseudo random part
+    const context = blake2bInit(8, null);
+    blake2bUpdate(context, random);
+    blake2bUpdate(context, hash);
+    const blakeRandom = blake2bFinal(context).reverse();
+    const check = threshold(blakeRandom);
+    if (check) return random.reverse();
+  }
+  return false;
+}
+
+onmessage = function (ev) {
+  for (let i = 0; i < 4096; i++) {
+    const generate = generator256(ev.data);
+    if (generate) {
+      postMessage(generate); // Worker return
+      break;
+    }
+  }
+  postMessage(false);
+};
 
 const powInitiate = (_threads, workerPath = '') => {
-  if (!window || !window.Worker || !window.navigator) return false;
   let threads = _threads;
   if (Number.isNaN(Number(threads))) {
-    threads = window.navigator.hardwareConcurrency - 1;
+    threads = cpus().length - 1;
   }
   const workers = [];
   for (let i = 0; i < threads; i += 1) {
@@ -25,7 +68,7 @@ const powThreshold = (Uint8Array) => {
   return false;
 };
 
-const powValidate = (powHex, hashHex) => {
+export const powValidate = (powHex, hashHex) => {
   if (!isValidHash(hashHex)) {
     throw new Error('Invalid: hash hex is not a valid hash');
   }
@@ -89,7 +132,7 @@ const powStart = (workers, hash) => {
   }
 };
 
-const pow = (hashHex, threads, callback, workerPath) => {
+export default (hashHex, threads, callback, workerPath) => {
   if (!isValidHash(hashHex)) {
     throw new Error('Invalid hash');
   }
