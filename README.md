92b8e1298d8b936d1755daff2471e3f9d20b3d90
SergiySW <sergiysw@gmail.com>
2018-01-26 15:46:21 +0300

    raiblockscommunity.net --> raiblocks.net

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ If your node/wallet version < 7.6.0 use file versions uploaded before 15/01/2017
 * **rai.rpc.js** contains very basic RPC request tool + all RPC commands as JS functions
 * ~~**rai.core.js** contains all RPC commands as JS functions~~ merged with rai.rpc.js
 * **rai.extended.js** contains some extended functions not available in current RPC implementation
-* **rai.community.js** contains requests to RaiBlocksCommunity.net website
+* **rai.community.js** contains requests to RaiBlocks.net website
 * **sample.html** RaiBlocksJS tech demo wallet skeleton
 
 # Usage sample
--- rai.community.js
+++ rai.community.js
@@ -6,7 +6,7 @@
 *
 *
 * RaiBlocks Community website
-* https://raiblockscommunity.net/
+* https://raiblocks.net/
 */
 
 
@@ -54,18 +54,18 @@ function RaiCommunity() {
 	
 
 	this.account = function(account) {
-		var account = this.json("https://raiblockscommunity.net/account/index.php?acc=" + account + "&json=1", JSON.stringify({}));
+		var account = this.json("https://raiblocks.net/account/index.php?acc=" + account + "&json=1", JSON.stringify({}));
 		return account;
 	}
 
 	this.block = function(hash) {
-		var block = this.json("https://raiblockscommunity.net/block/index.php?h=" + hash + "&json=1", JSON.stringify({}));
+		var block = this.json("https://raiblocks.net/block/index.php?h=" + hash + "&json=1", JSON.stringify({}));
 		return block;
 	}
 	
 	
 	this.frontiers = function() {
-		var frontiers = this.json("https://raiblockscommunity.net/page/frontiers.php?json=1", JSON.stringify({}));
+		var frontiers = this.json("https://raiblocks.net/page/frontiers.php?json=1", JSON.stringify({}));
 		return frontiers;
 	}
 	
@@ -77,19 +77,19 @@ function RaiCommunity() {
 	
 	
 	this.peers = function() {
-		var peers = this.json("https://raiblockscommunity.net/page/peers.php?json=1", JSON.stringify({}));
+		var peers = this.json("https://raiblocks.net/page/peers.php?json=1", JSON.stringify({}));
 		return peers;
 	}
 	
 	
 	this.representatives = function() {
-		var representatives = this.json("https://raiblockscommunity.net/page/representatives.php?json=1", JSON.stringify({}));
+		var representatives = this.json("https://raiblocks.net/page/representatives.php?json=1", JSON.stringify({}));
 		return representatives;
 	}
 	
 	
 	this.summary = function() {
-		var summary = this.json("https://raiblockscommunity.net/page/summary.php?json=1", JSON.stringify({}));
+		var summary = this.json("https://raiblocks.net/page/summary.php?json=1", JSON.stringify({}));
 		this.available_supply = summary.supply.available_supply;
 		this.block_count = summary.blocks;
 		this.frontier_count = summary.frontiers;
@@ -106,7 +106,7 @@ function RaiCommunity() {
 			let block = rai.block(this);
 			let url_block = encodeURIComponent(JSON.stringify(block));
 			let http = new XMLHttpRequest();
-			let url = "https://raiblockscommunity.net/processblock/elaborate.php";
+			let url = "https://raiblocks.net/processblock/elaborate.php";
 			let params = "processblock=" + url_block + "&submit=Process";
 			http.open("POST", url, true);
 			http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
